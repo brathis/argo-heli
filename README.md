1c27fe9a716206aee7e4a8ae77f48df95108d4f6
Mathis Dedial <mathis@dedial.net>
2024-10-23 16:23:28 +0000

    🅿️ use Angular

--- README.md
+++ README.md
@@ -2,24 +2,3 @@
 This is the website https://argo-heli.ch
 
 It is hosted on and deployed using Netlify: https://app.netlify.com/sites/argo-heli
-
-
-## Directory Layout
-```
-argo-heli/
-|
- - argo-heli/ # production app, not currently deployed
-|
- - parking/ # parking page, currently deployed
-|
- - README.md # this file
-```
-
-## Parking Page
-To preview the page from GitHub Codespace, the Python web server can be used:
-
-```bash
-python -m http.server --directory ./parking
-```
-
-This will serve the directory via HTTP on port 8000.
--- argo-heli/src/app/app.component.ts
+++ argo-heli/src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'argo-heli';
+export class AppComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    const targetDate = new Date('2024-11-01T00:00:00+01:00');
+    const elDays = document.querySelector('#days-dynamic');
+    const elMessage = document.querySelector('.message') as HTMLElement;
+    if (!elDays || !elMessage) {
+      return;
+    }
+    const remainingMilliseconds = targetDate.valueOf() - new Date().valueOf();
+    const remainingDays = Math.floor(remainingMilliseconds / 1000 / 3600 / 24);
+    elDays.innerHTML = remainingDays.toString();
+    elMessage.style.display = 'block';
+  }
 }
