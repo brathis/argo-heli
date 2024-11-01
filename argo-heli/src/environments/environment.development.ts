833765588edd57379feb8df1dc85a1fea47a2f08
Mathis Dedial <mathis@dedial.net>
2024-11-01 11:19:29 +0000

    🛠️ Setup separate build environments for dev and prod

---
+++ argo-heli/src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  bookingRequestEndpoint:
+    'https://ws1xf8s7la.execute-api.eu-central-2.amazonaws.com/Prod/booking-requests',
+};
---
+++ argo-heli/src/environments/environment.production.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  bookingRequestEndpoint:
+    'https://gy6d8bgc61.execute-api.eu-central-2.amazonaws.com/Prod/booking-requests',
+};
