57fc903b97046b263f3a2791100fbc4b04c3c41c
Mathis Dedial <mathis@dedial.net>
2024-11-02 13:56:47 +0000

    🚀 Replace countdown by business card

--- argo-heli/src/app/app.component.ts
+++ argo-heli/src/app/app.component.ts
@@ -1,23 +1,10 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { Component } from '@angular/core';
+import { LogoComponent } from './logo/logo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [LogoComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
 })
-export class AppComponent implements AfterViewInit {
-  ngAfterViewInit(): void {
-    const targetDate = new Date('2024-11-01T00:00:00+01:00');
-    const elDays = document.querySelector('#days-dynamic');
-    const elMessage = document.querySelector('.message') as HTMLElement;
-    if (!elDays || !elMessage) {
-      return;
-    }
-    const remainingMilliseconds = targetDate.valueOf() - new Date().valueOf();
-    const remainingDays = Math.floor(remainingMilliseconds / 1000 / 3600 / 24);
-    elDays.innerHTML = remainingDays.toString();
-    elMessage.style.display = 'block';
-  }
-}
+export class AppComponent {}
