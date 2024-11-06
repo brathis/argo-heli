6dbbb3a06cc07ef846fac02b5f1f9d11311976bb
Mathis Dedial <mathis@dedial.net>
2024-11-06 10:07:19 +0000

    🚀 Ensure that the selected date is in the future

--- argo-heli/src/app/page/booking/_presentational/flight-data/flight-data.component.ts
+++ argo-heli/src/app/page/booking/_presentational/flight-data/flight-data.component.ts
@@ -1,8 +1,10 @@
 import { Component, effect, input } from '@angular/core';
 import {
+  AbstractControl,
   FormControl,
   FormGroup,
   ReactiveFormsModule,
+  ValidationErrors,
   Validators,
 } from '@angular/forms';
 import { FormBlockComponent } from '../form-block/form-block.component';
@@ -29,7 +31,10 @@ export class FlightDataComponent {
     Validators.min(1),
     Validators.max(3),
   ]);
-  departureDateFormControl = new FormControl('', [Validators.required]);
+  departureDateFormControl = new FormControl('', [
+    Validators.required,
+    this.isFutureDateValidator,
+  ]);
   departureTimeFormControl = new FormControl('', [Validators.required]);
 
   group = input.required<FormGroup>();
@@ -52,5 +57,22 @@ export class FlightDataComponent {
       group.addControl('departureDate', this.departureDateFormControl);
       group.addControl('departureTime', this.departureTimeFormControl);
     });
+  }
+
+  private isFutureDateValidator(
+    control: AbstractControl,
+  ): ValidationErrors | null {
+    const value = new Date(control.value);
+    const date = new Date(
+      value.getFullYear(),
+      value.getMonth(),
+      value.getDate(),
+    );
+    const now = new Date();
+    const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+    if (date.valueOf() <= today.valueOf()) {
+      return { isNotFutureDate: { value: control.value } };
+    }
+    return null;
   }
 }
